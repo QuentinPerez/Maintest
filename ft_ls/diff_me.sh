8a9bba29eb76a6e51b2cea129e4a3c95116cd910
Stephane HERMAN <stherman@e2r9p7.42.fr>
2013-11-27 12:21:12 +0100

    bug correction

--- ft_ls/diff_me.sh
+++ ft_ls/diff_me.sh
@@ -6,7 +6,7 @@
 #    By: stherman <stherman@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2013/11/27 09:02:59 by stherman          #+#    #+#              #
-#    Updated: 2013/11/27 12:02:27 by stherman         ###   ########.fr        #
+#    Updated: 2013/11/27 12:20:21 by stherman         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -30,7 +30,7 @@ printf "# **********************************************************************
 
 printf "\033[0m"
 
-SUCCESS= ls -l t_dir/ > our && ft_ls -l t_dir > your
+SUCCESS= ls -l t_dir > our && ft_ls -l t_dir > your
 if !(diff --brief your our) || !($SUCCESS) then
 	printf "\r\n\033[31mError:\t\t\033[0m\033[37;1m ls -l\033[0m\n\n"
 	diff your our
@@ -38,7 +38,7 @@ else
 	printf "\n\033[32mSuccess:\t\033[37;1m ls -l\033[0m\n\n"
 fi
 
-SUCCESS= ls -a t_dir/ > our && ft_ls -a t_dir > your
+SUCCESS= ls -a t_dir > our && ft_ls -a t_dir > your
 if !(diff --brief your our) || !($SUCCESS) then
 	printf "\r\n\033[31mError:\t\t\033[0m\033[37;1m ls -a\033[0m\n\n"
 	diff your our
@@ -46,7 +46,7 @@ else
 	printf "\n\033[32mSuccess:\t\033[37;1m ls -a\033[0m\n\n"
 fi
 
-SUCCESS= ls -lr t_dir/ > our && ft_ls -r t_dir > your
+SUCCESS= ls -r t_dir > our && ft_ls -r t_dir > your
 if !(diff --brief your our) || !($SUCCESS) then
 	printf "\r\n\033[31mError:\t\t\033[0m\033[37;1m ls -r\033[0m\n\n"
 	diff your our
@@ -54,7 +54,7 @@ else
 	printf "\n\033[32mSuccess:\t\033[37;1m ls -r\033[0m\n\n"
 fi
 
-SUCCESS= ls -R t_dir/ > our && ft_ls -R t_dir > your
+SUCCESS= ls -R t_dir > our && ft_ls -R t_dir > your
 if !(diff --brief your our) || !($SUCCESS) then
 	printf "\r\n\033[31mError:\t\t\033[0m\033[37;1m ls -R\033[0m\n\n"
 	diff your our
@@ -62,7 +62,7 @@ else
 	printf "\n\033[32mSuccess:\t\033[37;1m ls -R\033[0m\n\n"
 fi
 
-SUCCESS= ls -t t_dir/ > our && ft_ls -t t_dir > your
+SUCCESS= ls -t t_dir > our && ft_ls -t t_dir > your
 if !(diff --brief your our) || !($SUCCESS) then
 	printf "\r\n\033[31mError:\t\t\033[0m\033[37;1m ls -t\033[0m\n\n"
 	diff your our
@@ -97,7 +97,7 @@ printf "# **********************************************************************
 
 printf "\033[0m"
 
-SUCCESS= ls -l -a t_dir/ > our && ft_ls -l -a t_dir > your
+SUCCESS= ls -l -a t_dir > our && ft_ls -l -a t_dir > your
 if !(diff --brief your our) || !($SUCCESS) then
     printf "\r\n\033[31mError:\t\t\033[0m\033[37;1m ls -l -a\033[0m\n\n"
     diff your our
@@ -105,7 +105,7 @@ else
     printf "\n\033[32mSuccess:\t\033[37;1m ls -l -a\033[0m\n\n"
 fi
 
-SUCCESS= ls -l -a -r t_dir/ > our && ft_ls -l -a -r t_dir > your
+SUCCESS= ls -l -a -r t_dir > our && ft_ls -l -a -r t_dir > your
 if !(diff --brief your our) || !($SUCCESS) then
     printf "\r\n\033[31mError:\t\t\033[0m\033[37;1m ls -l -a -r\033[0m\n\n"
     diff your our
@@ -113,7 +113,7 @@ else
     printf "\n\033[32mSuccess:\t\033[37;1m ls -l -a -r\033[0m\n\n"
 fi
 
-SUCCESS= ls -l -a -r -R t_dir/ > our && ft_ls -l -a -r -R t_dir > your
+SUCCESS= ls -l -a -r -R t_dir > our && ft_ls -l -a -r -R t_dir > your
 if !(diff --brief your our) || !($SUCCESS) then
     printf "\r\n\033[31mError:\t\t\033[0m\033[37;1m ls -l -a -r -R\033[0m\n\n"
     diff your our
@@ -121,7 +121,7 @@ else
     printf "\n\033[32mSuccess:\t\033[37;1m ls -l -a -r -R\033[0m\n\n"
 fi
 
-SUCCESS= ls -l -a -r -R -t t_dir/ > our && ft_ls -l -a -r -R -t t_dir > your
+SUCCESS= ls -l -a -r -R -t t_dir > our && ft_ls -l -a -r -R -t t_dir > your
 if !(diff --brief your our) || !($SUCCESS) then
     printf "\r\n\033[31mError:\t\t\033[0m\033[37;1m ls -l -a -r -R -t\033[0m\n\n"
     diff your our

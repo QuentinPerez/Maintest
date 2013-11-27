205ba36ac0455b68f9141614839147cc613645a6
Stephane HERMAN <stherman@e2r9p7.42.fr>
2013-11-27 12:03:45 +0100

    Ajout des tests multi-argument, ajout des balise pour la recherche dans le fichier

--- ft_ls/diff_me.sh
+++ ft_ls/diff_me.sh
@@ -6,7 +6,7 @@
 #    By: stherman <stherman@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2013/11/27 09:02:59 by stherman          #+#    #+#              #
-#    Updated: 2013/11/27 11:21:48 by stherman         ###   ########.fr        #
+#    Updated: 2013/11/27 12:02:27 by stherman         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -68,6 +68,65 @@ if !(diff --brief your our) || !($SUCCESS) then
 	diff your our
 else
 	printf "\n\033[32mSuccess:\t\033[37;1m ls -t\033[0m\n\n"
+fi
+
+printf "\n\n\033[1;37mPlease press a key to continue :D: \033[0m"
+read
+
+# -------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------- #
+# TEST_2        ------------------------------------------------------------------------------------------------ #
+# -------------------------------------------------------------------------------------------------------------- #
+# -------------------------------------------------------------------------------------------------------------- #
+
+clear
+
+printf "\033[44m\033[1;37m"
+
+printf "# **************************************************************************** #\n"
+printf "#                                                                              #\n"
+printf "#                                                         :::      ::::::::    #\n"
+printf "#                                                       :+:      :+:    :+:    #\n"
+printf "#                                                     +:+ +:+         +:+      #\n"
+printf "#      ~ TESTS WITH TWO OR MORE ARGUMENT ~          +#+  +:+       +#+         #\n"
+printf "#                                                 +#+#+#+#+#+   +#+            #\n"
+printf "#                                                      #+#    #+#              #\n"
+printf "#                                                     ###   ########.fr        #\n"
+printf "#                                                                              #\n"
+printf "# **************************************************************************** #\n\n\n"
+
+printf "\033[0m"
+
+SUCCESS= ls -l -a t_dir/ > our && ft_ls -l -a t_dir > your
+if !(diff --brief your our) || !($SUCCESS) then
+    printf "\r\n\033[31mError:\t\t\033[0m\033[37;1m ls -l -a\033[0m\n\n"
+    diff your our
+else
+    printf "\n\033[32mSuccess:\t\033[37;1m ls -l -a\033[0m\n\n"
+fi
+
+SUCCESS= ls -l -a -r t_dir/ > our && ft_ls -l -a -r t_dir > your
+if !(diff --brief your our) || !($SUCCESS) then
+    printf "\r\n\033[31mError:\t\t\033[0m\033[37;1m ls -l -a -r\033[0m\n\n"
+    diff your our
+else
+    printf "\n\033[32mSuccess:\t\033[37;1m ls -l -a -r\033[0m\n\n"
+fi
+
+SUCCESS= ls -l -a -r -R t_dir/ > our && ft_ls -l -a -r -R t_dir > your
+if !(diff --brief your our) || !($SUCCESS) then
+    printf "\r\n\033[31mError:\t\t\033[0m\033[37;1m ls -l -a -r -R\033[0m\n\n"
+    diff your our
+else
+    printf "\n\033[32mSuccess:\t\033[37;1m ls -l -a -r -R\033[0m\n\n"
+fi
+
+SUCCESS= ls -l -a -r -R -t t_dir/ > our && ft_ls -l -a -r -R -t t_dir > your
+if !(diff --brief your our) || !($SUCCESS) then
+    printf "\r\n\033[31mError:\t\t\033[0m\033[37;1m ls -l -a -r -R -t\033[0m\n\n"
+    diff your our
+else
+    printf "\n\033[32mSuccess:\t\033[37;1m ls -l -a -r -R -t\033[0m\n\n"
 fi
 
 rm -f your

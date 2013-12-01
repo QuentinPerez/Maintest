bce27b6cc58d42083922e56af0a7e23c9ee9b6bb
Quentin PEREZ <qperez42@gmail.com>
2013-12-01 23:13:48 +0100

    [add diff ]

--- getnextline/main.c
+++ getnextline/main.c
@@ -6,7 +6,7 @@
 /*   By: student@42 <@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/01 22:10:06 by student@42        #+#    #+#             */
-/*   Updated: 2013/12/01 22:35:11 by qperez           ###   ########.fr       */
+/*   Updated: 2013/12/01 23:13:20 by qperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,9 +23,11 @@ int	main(int argc, char const** argv)
 	int		fd;
 	int		fd2;
 	char	*line;
+	pid_t	child;
 
 	fd = open("cmp.txt", O_RDONLY);
 	fd2 = open("me.txt", O_WRONLY | O_CREAT);
+	printf("HELLO\n");
 	if (fd == -1 || fd2 == -1)
 	{
 		perror("open");
@@ -40,6 +42,19 @@ int	main(int argc, char const** argv)
 	}
 	close(fd);
 	close(fd2);
+	child = fork();
+	if (child == 0)
+	{
+		char	*arg[] = {"cmp.txt", "me.txt", NULL};
+
+		execve("/usr/bin/diff", arg, NULL);
+		exit(0);
+	}
+	else
+	{
+		while (waitpid(0, NULL, 0) != 0) // bad code I know ... but it's not the project
+			;
+	}
 	(void)argc;
 	(void)argv;
 	return (0);

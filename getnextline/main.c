2f768695856cfe5278a41c58de689c0383aaad10
Quentin PEREZ <qperez42@gmail.com>
2013-12-03 13:36:23 +0100

    [add argv]

--- getnextline/main.c
+++ getnextline/main.c
@@ -6,7 +6,7 @@
 /*   By: student@42 <@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2013/12/01 22:10:06 by student@42        #+#    #+#             */
-/*   Updated: 2013/12/03 11:27:55 by qperez           ###   ########.fr       */
+/*   Updated: 2013/12/03 13:35:51 by qperez           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 
 int	get_next_line(int fd, char **line);
 
-int	main(int argc, char const** argv)
+int	main(int argc, char ** argv)
 {
 	int		fd;
 	int		fd2;
@@ -26,8 +26,13 @@ int	main(int argc, char const** argv)
 	pid_t	child;
 	char	n = '\n';
 
-	fd = open("cmp.txt", O_RDONLY);
-	fd2 = open("me.txt", O_WRONLY | O_CREAT, 0666);
+	if (argc < 2)
+	{
+		printf("Usage %s <filename>\n", argv[0]);
+		return (1);
+	}
+	fd = open(argv[1], O_RDONLY);
+	fd2 = open("me.txt", O_WRONLY | O_CREAT | O_TRUNC, 0666);
 	if (fd == -1 || fd2 == -1)
 	{
 		perror("open");
@@ -38,16 +43,17 @@ int	main(int argc, char const** argv)
 	while (get_next_line(fd, &line) == 1)
 	{
 		write(fd2, line, strlen(line));
-		write(fd2, &n, 1);
-		free(line);
+		write(fd2, &n, 1);					// attention si le fichier test n a pas de \n et que ca affiche une erreur c'est normal
+		free(line);							// vous inquietez pas
 	}
 	close(fd);
 	close(fd2);
 	child = fork();
 	if (child == 0)
 	{
-		char	*arg[] = {"/usr/bin/diff", "cmp.txt", "me.txt", NULL};
+		char	*arg[] = {"/usr/bin/diff", NULL, "me.txt", NULL};
 
+		arg[1] = argv[1];
 		execve(arg[0], arg, NULL);
 		exit(0);
 	}
